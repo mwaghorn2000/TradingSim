1087c7a5280406c26681b6cf5e76574d8eca885b
mitch <m.waghorn2000@gmail.com>
2024-05-15 13:44:04 +1000

    Added ENV to CMakeLists.txt

--- cmake-build-debug/.qt/QtDeployTargets.cmake
+++ cmake-build-debug/.qt/QtDeployTargets.cmake
@@ -1,2 +1,1 @@
-set(__QT_DEPLOY_TARGET_TradingSim_FILE C:/Users/geazy/CLionProjects/TradingSim/cmake-build-debug/TradingSim.exe)
-set(__QT_DEPLOY_TARGET_TradingSim_RUNTIME_DLLS C:/Qt/6.7.0/mingw_64/bin/Qt6Charts.dll;C:/Qt/6.7.0/mingw_64/bin/Qt6OpenGLWidgets.dll;C:/Qt/6.7.0/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.7.0/mingw_64/bin/Qt6OpenGL.dll;C:/Qt/6.7.0/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.7.0/mingw_64/bin/Qt6Core.dll)
+set(__QT_DEPLOY_TARGET_TradingSim_FILE /home/mitch/Projects/TradingSim/cmake-build-debug/TradingSim)
